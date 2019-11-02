426ae62590d49abe46d633f7f2b3cdd59db38e9f
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-11-02 19:12:11 +0000

    Implement `AsJValue` for primitive types

--- jnix/src/as_jvalue.rs
+++ jnix/src/as_jvalue.rs
@@ -20,3 +20,20 @@ impl<'env_borrow, 'env: 'env_borrow> AsJValue<'env> for AutoLocal<'env, 'env_bor
         JValue::Object(self.as_obj())
     }
 }
+
+macro_rules! impl_for_primitives {
+    ( $( $primitive:ty ),* $(,)* ) => {
+        $(
+            impl<'env> AsJValue<'env> for $primitive {
+                fn as_jvalue<'borrow>(&'borrow self) -> JValue<'borrow>
+                where
+                    'env: 'borrow,
+                {
+                    JValue::from(*self)
+                }
+            }
+        )*
+    };
+}
+
+impl_for_primitives!((), bool, u8, i8, u16, i16, i32, i64, f32, f64);
