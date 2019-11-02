b067e0631205eb45f4c064c4d394692e855066ef
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-11-02 19:31:35 +0000

    Create `IntoJava` trait

--- jnix/src/lib.rs
+++ jnix/src/lib.rs
@@ -8,6 +8,7 @@
 pub extern crate jni;
 
 mod as_jvalue;
+mod into_java;
 mod jnix_env;
 
-pub use self::{as_jvalue::AsJValue, jnix_env::JnixEnv};
+pub use self::{as_jvalue::AsJValue, into_java::IntoJava, jnix_env::JnixEnv};
