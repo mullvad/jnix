11271213698d002a3627143081c67969f4c1caed
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-11-22 12:04:59 +0000

    Improve documentation

--- jnix-macros/src/lib.rs
+++ jnix-macros/src/lib.rs
@@ -1,5 +1,8 @@
-//! This is a companion crate to `jnix` that provides some procedural macros for interfacing JNI
-//! with Rust.
+//! This is a companion crate to [`jnix`] that provides some procedural macros for interfacing JNI
+//! with Rust. See the [`jnix` crate documentation][doc] for more information
+//!
+//! [`jnix`]: https://crates.io/crates/jnix
+//! [doc]: https://docs.rs/jnix/
 
 #![deny(missing_docs)]
 
