725871cf8e081cf42052431ee0966e09499dfa4e
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-01-08 14:56:57 +0000

    Implement blanket `FromJava<JValue>`

--- src/from_java/implementations/std.rs
+++ src/from_java/implementations/std.rs
@@ -1,5 +1,23 @@
 use crate::{FromJava, JnixEnv};
-use jni::objects::{AutoLocal, JObject};
+use jni::objects::{AutoLocal, JObject, JValue};
+
+impl<'env, 'sub_env, T> FromJava<'env, JValue<'sub_env>> for T
+where
+    'env: 'sub_env,
+    T: FromJava<'env, JObject<'sub_env>>,
+{
+    const JNI_SIGNATURE: &'static str = T::JNI_SIGNATURE;
+
+    fn from_java(env: &JnixEnv<'env>, source: JValue<'sub_env>) -> Self {
+        match source {
+            JValue::Object(object) => T::from_java(env, object),
+            _ => panic!(
+                "Can't convert non-object Java type. Expected type signature {}",
+                Self::JNI_SIGNATURE
+            ),
+        }
+    }
+}
 
 impl<'env, 'sub_env, 'borrow, T> FromJava<'env, AutoLocal<'sub_env, 'borrow>> for T
 where
